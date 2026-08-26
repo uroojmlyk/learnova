0a836115253dcde546d30f6252836fce848ff8a0
Urooj Rasheed <uroojr335@gmail.com>
2026-08-26 21:37:30 +0500

    add gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,8 +79,11 @@
 
 
 
+
+
 import './globals.css'
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Nunito } from 'next/font/google'
 import Navbar from '@/components/shared/Navbar'
 import Footer from '@/components/shared/Footer'
@@ -145,11 +148,37 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={nunito.variable}>
       <body className={`${nunito.className} min-h-screen flex flex-col bg-[#FFFDF7]`}>
-         <StructuredData />
+
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P3ZVQLFN');
+          `}
+        </Script>
+
+        <StructuredData />
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
         <WhatsAppButton />
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P3ZVQLFN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
       </body>
     </html>
   )
